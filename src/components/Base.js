f1d6028710cd6cfd71b6fe19d8c64959148e9e63
SKN <skn437physx@gmail.com>
2022-11-28 03:11:14 +0600

    Framer Variants

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,34 +1,93 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+	initial: {
+		opacity: 0,
+		x: "100vw",
+	},
+	animate: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			type: "spring",
+			//delay: 0.5,
+			stiffness: 120,
+		},
+	},
+};
+
+const nextVariant = {
+	initial: {
+		x: "-100vw",
+	},
+	animate: {
+		x: 0,
+		transition: {
+			type: "spring",
+			stiffness: 120,
+		},
+	},
+};
 
 const Base = ({ addBase, pizza }) => {
-  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+	const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
-  return (
-    <div className="base container">
+	return (
+		<motion.div
+			className="base container"
+			variants={containerVariants}
+			initial="initial"
+			animate="animate"
+		>
+			<h3>Step 1: Choose Your Base</h3>
+			<ul>
+				{bases.map((base) => {
+					let spanClass = pizza.base === base ? "active" : "";
+					return (
+						<motion.li
+							key={base}
+							onClick={() => addBase(base)}
+							whileHover={{
+								scale: 1.3,
+								color: "#f8e112",
+								originX: 0,
+							}}
+							transition={{
+								type: "spring",
+								stiffness: 300,
+							}}
+						>
+							<span className={spanClass}>{base}</span>
+						</motion.li>
+					);
+				})}
+			</ul>
 
-      <h3>Step 1: Choose Your Base</h3>
-      <ul>
-        {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
-          return (
-            <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
-            </li>
-          )
-        })}
-      </ul>
-
-      {pizza.base && (
-        <div className="next">
-          <Link to="/toppings">
-            <button>Next</button>
-          </Link>
-        </div>
-      )}
-
-    </div>
-  )
-}
+			{pizza.base && (
+				<motion.div
+					className="next"
+					variants={nextVariant}
+					// initial="initial"
+					// animate="animate"
+				>
+					<Link to="/toppings">
+						<motion.button
+							whileHover={{
+								scale: 1.1,
+								textShadow: "0 0 8px 0 rgb(255, 255, 255)",
+								boxShadow: "0 0 8px 0 rgb(255, 255, 255)",
+							}}
+						>
+							Next
+						</motion.button>
+					</Link>
+				</motion.div>
+			)}
+		</motion.div>
+	);
+};
 
 export default Base;
+
